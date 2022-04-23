432afbedfa48fbfb70ac89f742f3d1ec455349d2
root <root@bus.local>
2022-04-23 18:55:32 +0300

    first commit

---
+++ entrypoint.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+git clone https://github.com/klimoffgod/asterisk.git && cd /asterisk/v1 && sh install_conf.sh
---
+++ install_conf.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+aptitude install  -y sox screen dialog  apt-utils
+cp /asterisk/v1/asterisk/*  /etc/asterisk
+cp /asterisk/v1/sounds/*  /var/lib/asterisk/sounds
+screen service asterisk start
